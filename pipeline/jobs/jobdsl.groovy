c9e9eb48c2f2b2d539cb14990a64b75626243199
Ubuntu <ubuntu@ip-172-31-0-135.us-west-2.compute.internal>
2016-02-17 17:53:45 +0000

    added run-integration-tests

--- pipeline/jobs/jobdsl.groovy
+++ pipeline/jobs/jobdsl.groovy
@@ -31,7 +31,7 @@ def create_view(pipeline, triggerjob) {
 def pipelines =  [
   "Continuous Delivery Pipeline":[
     "commit":["trigger", "commit"],
-    "acceptance": ["create-environment-acceptance", "run-infrastructure-tests", "terminate-environment-acceptance"],
+    "acceptance": ["create-environment-acceptance", "run-infrastructure-tests", "run-integration-tests", "terminate-environment-acceptance"],
     "exploratory" : ["approve-reject-exploratory"],
     "capacity" : ["launch-environment-capacity", "performance-testing", "load-testing", "stress-testing", "penetration-testing"],
     "production" : ["approve-reject-prod", "launch-prod-environment", "blue-green-deployment"]
